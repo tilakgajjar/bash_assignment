2b721661f53409de922f007f74fd55310cf611cc
Tilak Gajjar <tilak@Tilaks-MacBook-Pro.local>
2017-11-28 18:16:34 -0500

    Adding assignment.sh

---
+++ assignment.sh
@@ -0,0 +1,10 @@
+mkdir -p results
+touch results/date.txt
+echo "Today is $(date)" > date.txt
+cat date.txt
+touch results/complete.txt
+echo “Assignment Complete” > complete.txt
+cat complete.txt
+
+
+
