ac78c563c1339181cf13a1d4339e355e72451bf6
Tilak Gajjar <tilak@Tilaks-MacBook-Pro.local>
2017-11-28 18:21:30 -0500

    Changes in assignment.sh

--- assignment.sh
+++ assignment.sh
@@ -3,7 +3,7 @@ touch results/date.txt
 echo "Today is $(date)" > date.txt
 cat date.txt
 touch results/complete.txt
-echo “Assignment Complete” > complete.txt
+echo “New Branch Created” > complete.txt
 cat complete.txt
 
 
